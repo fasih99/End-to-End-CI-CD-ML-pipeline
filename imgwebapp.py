cd94dd66d972663e15b947d4a39a410de213cfaa
fasih99 <59806488+fasih99@users.noreply.github.com>
2022-05-09 17:06:57 +0530

    Update imgwebapp.py

--- imgwebapp.py
+++ imgwebapp.py
@@ -13,6 +13,10 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 st.title("Potato Image Classifier")
 st.text("Provide an image of Potato Leaf")
 
+menu = ["Image","Camera"]
+choice = st.sidebar.selectbox("Menu",menu)
+
+
 @st.cache(allow_output_mutation=True)
 def load_model():
   model = tf.keras.models.load_model('/Users/fasihussaini/Desktop/PlantDiseaseClassifierDemo/models/1')
@@ -24,29 +28,41 @@ with st.spinner('Loading Model Into Memory....'):
 classes=['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
 
 
-file= st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-if file is not None:
+if choice == "Image":
+    st.subheader("Image")
+    file= st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    if file is not None:
 
-    image= Image.open(file)
+        image= Image.open(file)
     
-    img_array = np.array(image)
+        img_array = np.array(image)
     
-    img_tensor = tf.cast(img_array, tf.float32)
-    img = tf.image.resize(img_tensor,[256,256])
-    img = np.expand_dims(img, axis = 0)
-    
-    st.write("Predicted Class :")
-    with st.spinner('classifying.....'):
-      pred = model.predict(img)
-      
-      label =np.argmax(pred,axis=1)
-      
-      confidence = "{:.2f}".format(100*np.max(pred))
-      
-      st.write(classes[label[0]]) 
-      
-      st.write("Confidence :", confidence) 
-    st.write("")
-    image = Image.open(file)
-    st.image(image, caption='Classifying Potato Image', width=400)
+        img_tensor = tf.cast(img_array, tf.float32)
+        img = tf.image.resize(img_tensor,[256,256])
+        img = np.expand_dims(img, axis = 0)
+        
+        st.write("Predicted Class :")
+        with st.spinner('classifying.....'):
+          pred = model.predict(img)
+          
+          label =np.argmax(pred,axis=1)
+          
+          confidence = "{:.2f}".format(100*np.max(pred))
+          
+          st.write(classes[label[0]]) 
+          
+          st.write("Confidence :", confidence) 
+          st.write("")
+          image = Image.open(file)
+          st.image(image, caption='Classifying Potato Image', width=400)
+
+
+elif choice == "Camera":
+  st.subheader("Camera")
+  
+  picture = st.camera_input("Take a picture")
+
+  if picture:
+      st.image(picture)
+
 
